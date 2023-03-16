a71225a40faa7485ddb4a85c60f411cb87737901
irafajs <irafajs@gmail.com>
2023-03-16 15:27:45 -0700

    HW4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main entry point
+ *
+ * return always 0 after running the code
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
