81a7d2aa8db660f17fba1f7899b7e8ca754e6d34
irafajs <irafajs@gmail.com>
2023-03-19 10:55:58 -0700

    VIEWT9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a = 0;
+	int sep = 0, sep1 = 0;
+	sep = ',';
+	sep1 = ' ';
+
+	while (a < 10)
+	{
+		putchar('0' + a);
+		++a;
+		putchar(sep);
+		putchar(sep1);
+	}
+	putchar('\n');
+	return (0);
+}
