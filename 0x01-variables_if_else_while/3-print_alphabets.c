3fb2855d4a35049ce7acddc6d958da4f4bf03a48
irafajs <irafajs@gmail.com>
2023-03-19 03:49:47 -0700

    VIEWT3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	for (lc = 'A'; lc <= 'Z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+return (0);
+}
