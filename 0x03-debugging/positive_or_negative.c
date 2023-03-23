353ea314046fb06a76a7788f2197295d8f67269c
irafajs <irafajs@gmail.com>
2023-03-23 11:37:43 -0700

    DT0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,9 +1,18 @@
 #include "main.h"
 
+/**
+*positive_or_negative - Entry point
+*@i : par for function
+*
+*description : 'check if a number is neg'
+*
+*Return: Always 0 (Success)
+*/
+
 void positive_or_negative(int i)
 {
 	int a = 98;
-	
+
 	if (a > 0)
 	{
 		printf("%d is positive\n", i);
