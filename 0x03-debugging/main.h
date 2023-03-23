54205542c22cec05635b285259ba0a6d4b95a3c3
irafajs <irafajs@gmail.com>
2023-03-23 07:38:01 -0700

    DT0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <unistd.h>
 #include <stdio.h>
 
-void positive_or_negative(int i);
+void positive_or_negative(void);
 
 #endif /* MAIN_H */
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void positive_or_negative(void)
+{
+	int i = 0;
+	
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else 
+	{
+		printf("%d is zero\n", i);
+	}
+}
