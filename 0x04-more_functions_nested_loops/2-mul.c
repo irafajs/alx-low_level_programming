f034e7a90b7d91d930674661c05b035ab0232ad0
irafajs <irafajs@gmail.com>
2023-03-24 13:30:10 -0700

    MFNLT2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*mul - multiply a andb
+*@a: accept single value
+*@b: accept single value
+*
+*Description: funtion read vlaue a, b and produce
+*multiplication of 2 number.
+*
+*Return: multiplication of a and b.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
