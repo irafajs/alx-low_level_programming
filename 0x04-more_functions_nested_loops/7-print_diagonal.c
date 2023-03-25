f50dc6ddab4d5c626aabb68a65b7eaac0655173e
irafajs <irafajs@gmail.com>
2023-03-25 09:22:54 -0700

    MFNLT7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,6 +13,12 @@ void print_diagonal(int n)
 	int x;
 	int y;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
 	for (x = 0; x < n; x++)
 	{
 		for (y = 0; y < x; y++)
@@ -21,5 +27,6 @@ void print_diagonal(int n)
 		}
 		_putchar('\\');
 		_putchar('\n');
+	}
 	}
 }
