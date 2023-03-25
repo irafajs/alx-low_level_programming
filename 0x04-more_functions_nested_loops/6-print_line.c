69be49ad213a90d7a03dc9689d8564d55c19bd93
irafajs <irafajs@gmail.com>
2023-03-25 05:32:37 -0700

    MFNLT6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+* print_line - print n line
+*@n: argument passed to function
+*
+* retun: 0 when successful
+*/
+
 void print_line(int n)
 {
 	int i;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		i++;
 		_putchar('_');
 	}
 	_putchar('\n');
