67badf26856ef82b03977117fae77a8619249619
irafajs <irafajs@gmail.com>
2023-03-27 19:56:05 +0000

    PAST1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ * swap_int - swap variable using pointer
+ * @a: passed parameter
+ * @b: passed parameter
+ *
+ * return: 0 at success
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
