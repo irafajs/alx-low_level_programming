84490208c550ddce4d05f75b82515c6cf18e738e
irafajs <irafajs@gmail.com>
2023-03-29 17:09:38 +0000

    MPAST2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+/**
+ *_strncpy - output a copy of the string src to dest
+ *@dest: passed param dest
+ *@src: passed param src
+ *@n: passed param
+ *
+ * Return: 0 at success
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	while (a < n)
+	{
+		a++;
+		dest[a] = '\0';
+	}
+	return (dest);
+}
