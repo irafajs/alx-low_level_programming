924a25a767b5fdc155c3f60af75944e6ef6d87a9
irafajs <irafajs@gmail.com>
2023-03-29 16:16:23 +0000

    MPAST0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+/**
+ * _strcat - concatinate  source to destination
+ *@dest: passed argument to destination
+ *@src: passed argumnet to source
+ *
+ *Return: 0 at success
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{}
+	for (b = 0; src[b] != '\0'; b++)
+	{}
+	dest[a] = src[b];
+	return (dest);
+}
