82fa351a523b519dd74e7a985a37ddb45c387f96
irafajs <irafajs@gmail.com>
2023-03-29 17:13:59 +0000

    MPAST0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,8 +13,11 @@ char *_strcat(char *dest, char *src)
 
 	for (a = 0; dest[a] != '\0'; a++)
 	{}
+
 	for (b = 0; src[b] != '\0'; b++)
-	{}
+	{
 	dest[a] = src[b];
+	}
+	dest[a + b] = '\0';
 	return (dest);
 }
