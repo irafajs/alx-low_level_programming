409e04e7c3041507a8b8ff57e0384c069002c384
irafajs <irafajs@gmail.com>
2023-03-30 00:23:07 +0000

    MPAST5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,16 @@
+/**
+ *string_toupper - change output to upper
+ *@str: passed param to func
+ *
+ *Return: 0 at success
+ */
 char *string_toupper(char *str)
 {
 	int m;
 
 	for (m = 0; str[m] != '\0'; m++)
 	{
-		if(str[m] >= 'a' && str[m] <= 'z')
+		if (str[m] >= 'a' && str[m] <= 'z')
 		{
 			str[m] = str[m] - 32;
 		}
