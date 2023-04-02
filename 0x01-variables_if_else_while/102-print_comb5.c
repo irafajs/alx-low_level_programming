4d9cb06fbd5629e6254f8859cafd0c28b882aa70
irafajs <irafajs@gmail.com>
2023-04-02 05:30:58 +0000

    VIET102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - compbo of 4;
+ *
+ * Return: 0 at success
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (b >= d)
+					{
+						continue;
+					}
+					putchar (a + '0');
+					putchar (b + '0');
+					putchar (' ');
+					putchar (c + '0');
+					putchar (d + '0');
+					if (a == 9 && b == 8 && c == 9 && d == 9)
+					{
+						continue;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
