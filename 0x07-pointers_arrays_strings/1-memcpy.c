28cd01674c2745a8b2c8dcaeba2997dad44554cc
irafajs <irafajs@gmail.com>
2023-04-03 19:27:43 +0000

    PAST0A1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+/**
+ * _memcpy - output a copied block of char
+ * @dest: destination add
+ * @src: source address
+ * @n: passed param of code to be copied
+ *
+ * Return: 0 at success
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contain exercises about pointer, array and strings.
