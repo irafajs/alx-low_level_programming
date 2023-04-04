c0be0dbdec95aa9dc89cdfc98cb916b1512cc1b2
irafajs <irafajs@gmail.com>
2023-04-04 21:01:52 +0000

    MPAST6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stddef.h>
 
+/**
+ * print_chessboard - output the chessboard keys name
+ * @a: point to the char
+ *
+ * Return: 0 at success
+ */
 void print_chessboard(char (*a)[8])
 {
 	int b, c;
