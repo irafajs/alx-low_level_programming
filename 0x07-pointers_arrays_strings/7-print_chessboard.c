83a4b3624f179896f08503736ff4bbcbeba2dcaa
irafajs <irafajs@gmail.com>
2023-04-04 20:58:29 +0000

    MPAST6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stddef.h>
+
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	for (b = 0; b <= 7; b++)
+	{
+		for (c = 0; c <= 7; c++)
+		{
+		_putchar(a[b][c]);
+		}
+		_putchar('\n');
+	}
+}
