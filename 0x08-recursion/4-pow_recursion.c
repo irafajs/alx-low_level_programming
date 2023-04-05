970c83005b73407f7ea75fdc3b4f8a7fbee3e92b
irafajs <irafajs@gmail.com>
2023-04-05 21:57:09 +0000

    RT04

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - output x power y
+ * @x: passed param to power on
+ * @y: passed param to multiply x
+ *
+ * Return: 0 at success
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y <= 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
