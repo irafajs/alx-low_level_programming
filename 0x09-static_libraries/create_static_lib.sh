3d72600f3f8474b871121bf2d882228e90aed463
irafajs <irafajs@gmail.com>
2023-04-09 09:44:13 +0000

    SL01C

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c
-=======
-gcc -c *.c
->>>>>>> 36f7ae0a60f3ca360125552d653236111535a963
-ar rc liball.a *.c
+ar rc liball.a *.o
