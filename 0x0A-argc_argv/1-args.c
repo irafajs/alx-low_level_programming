50f00139305f7ff222766bd740f8205ad54bc181
irafajs <irafajs@gmail.com>
2023-04-10 09:06:48 +0000

    AA01

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - output the passed argument in counted number
+ * @argc: passed argument counted in #
+ * @argv: passed 1d array
+ *
+ * Return: 0 at success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc -1);
+	return (0);
+}
