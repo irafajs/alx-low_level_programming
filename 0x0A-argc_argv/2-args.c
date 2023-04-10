123adcac5d320b702d7d21c027af7d0fea71eaa1
irafajs <irafajs@gmail.com>
2023-04-10 10:04:46 +0000

    AA02

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * main - output the passed param
+ * @argc: passed counted arguments
+ * @argv: passed 1d array arguments
+ *
+ * Return: 0 at success
+ */
+int main (int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
