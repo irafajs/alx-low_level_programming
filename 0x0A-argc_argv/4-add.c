66319862ced24a7d1a25674636dfbba49ec98b6a
irafajs <irafajs@gmail.com>
2023-04-10 12:17:21 +0000

    AA04

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - output the multiplication of 2 passed argv
+ * @argc: passed argument as a counter
+ * @argv: passed argument as a 1d array
+ *
+ * Return: 0 at success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, add;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!(isdigit(*argv[i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			add += atoi(argv[i]);
+		}
+			printf("%i\n", add);
+	}
+	return (0);
+}
+
+
+
