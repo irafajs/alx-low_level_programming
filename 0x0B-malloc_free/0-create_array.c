420a0494568797e1c5085d06754ac2b4149f7a96
irafajs <irafajs@gmail.com>
2023-04-11 18:14:03 +0000

    MF01

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,15 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *hexa;
 
+	if (size <= 0)
+	{
+		return (NULL);
+	}
 	hexa = malloc(sizeof(char) * size);
+	if (hexa == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		hexa[i] = c;
