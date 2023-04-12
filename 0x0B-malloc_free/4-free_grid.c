b19549b4654167810b648988c5181138a4162e25
irafajs <irafajs@gmail.com>
2023-04-12 16:18:01 +0000

    MF04

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * free_grid - output the grid info
+ * @grid: passed argument as ap to p
+ * @height: passed argument as heiight
+ *
+ * Return: void at success
+ */
 void free_grid(int **grid, int height)
 {
 	int a;
 
 	a = 0;
-	while(a < height)
+	while (a < height)
 	{
 		a++;
 		free(*(grid + a));
