074dadf7cd2243b75a0783f25f26af91ecf3f54b
irafajs <irafajs@gmail.com>
2023-04-14 01:15:14 +0000

    MMF03

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * array_range - output array of int
+ * @min: passed param
+ * @max: passed param
+ *
+ * Return: pointer at success, NULL if fails
+ */
 int *array_range(int min, int max)
 {
 	int *ab;
