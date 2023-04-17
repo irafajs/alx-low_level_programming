80aa0f7403fe7520be048096b17c2da4408e764e
irafajs <irafajs@gmail.com>
2023-04-17 22:21:50 +0000

    CPP03

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef _FUNCTION_LIKE_MACRO
-#define _FUNCTION_LIKE_MACRO
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
 #include <stdlib.h>
 #include <inttypes.h>
 #define ABS(x) ((x) < 0 ? (-x) : (x))
