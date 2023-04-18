981db3231fd0c5b5f5322d9c6d6a1583b847fbcc
irafajs <irafajs@gmail.com>
2023-04-18 00:22:38 +0000

    CPP01

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,10 +1,19 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * init_dog - output the assigned value of passed param
+ * @d: pointer to struct dog
+ * @name: passed param to name
+ * @age: passed param to age
+ * @owner: passed param to owner
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-		return ;
+		return;
 }
