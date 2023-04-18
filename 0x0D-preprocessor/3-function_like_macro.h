b3d3188de6d63685676759914b0e2bfcad83da55
irafajs <irafajs@gmail.com>
2023-04-18 16:44:36 +0000

    CPP3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,5 +2,5 @@
 #define _FUNCTION_LIKE_MACRO_H
 #include <stdlib.h>
 #include <inttypes.h>
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 #endif /*end of double inclusion check */
