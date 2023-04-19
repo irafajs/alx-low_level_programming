a7393af61e85bbf8a0c3c8a620fb85a354664eba
irafajs <irafajs@gmail.com>
2023-04-19 16:42:18 +0000

    FP01

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - output the array pointed to by a function pointer
+ * @array: passed array
+ * @size: passed unsigned int
+ * @action: passed fucntion pointer
+ *
+ * Return : nothing at success
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int a;
+
+	for (a = 0; a < size; a++)
+	{
+		action(array[a]);
+	}
+}
