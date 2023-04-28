4480a64f81660f6f85ca95cede4577974ff36f50
irafajs <irafajs@gmail.com>
2023-04-28 00:16:23 +0000

    SLL02

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,37 +1,28 @@
 #include "lists.h"
+
+/**
+ * _adjcfunc - calculate the stringth lenght
+ * @k: passed argument as pointer
+ *
+ * Return : int at success
+ */
 int _adjcfunc(const char *k)
 {
 	int a = 0;
+
 	while (k[a] != '\0')
 	{
 		a++;
 	}
 	return (a);
 }
-char *point2next(const char *str)
-{
-	int b = 0, a;
-	char *k;
-
-	if (str == NULL)
-	{
-		return (0);
-	}
-	while (*(str + 1))
-	{
-		a++;
-	}
-	k = malloc(sizeof(char) * 1 + 1);
-	if (k == 0)
-	{
-		return (0);
-	}
-	for ( a = 0; a <= b; a++)
-	{
-		*(k + a) = *(str + a);
-	}
-	return (k);
-}
+/**
+ * add_node - write out the node of a list
+ * @head: head ppointer to the next mem of a node
+ * @str: passed arg
+ *
+ * Return: a pointer at success
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
