6b7c3dd1049af000fabd31261d23014131f49d88
irafajs <irafajs@gmail.com>
2023-04-28 11:59:14 +0000

    SLL05

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * printbmain - write string before main
+ *
+ * Return: nothing
+ */
+void printbmain(void)
+{
+	char p1[] = "You're beat! and yet, you must allow,";
+	char p2[] = "\nI bore my house upon my back!\n";
+
+	printf("%s%s", p1, p2);
+
+}
