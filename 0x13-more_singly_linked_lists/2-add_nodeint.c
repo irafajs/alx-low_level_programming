3af522992ce148930e89c9356e8a13335dea0dfb
irafajs <irafajs@gmail.com>
2023-04-30 15:48:25 +0000

    MSLL02

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 
-size_t print_listint(const listint_t *h)
+/**
+ * add_nodeint - write out the node at beginning of a list
+ * @head: passed arg as pointer to a pointer
+ * @n: passed int as data handler
+ *
+ * Return: pointer at success
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	
+	listint_t *temp;
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	temp->n = n;
+	temp->next = *head;
+	*head = temp;
+	return (temp);
+}
+
