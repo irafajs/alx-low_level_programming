e0cebc6756e4d62b36f03216f65f35255a439569
irafajs <irafajs@gmail.com>
2023-04-30 22:24:01 +0000

    MSLL04

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,10 +1,16 @@
 #include "lists.h"
 
+/**
+ * free_listint - free the allocated memory of function
+ * @head: passed ard ad a pointer
+ *
+ * Return: nothing
+ */
 void free_listint(listint_t *head)
 {
-	listint_t * trav = NULL;
+	listint_t *trav = NULL;
 
-	while(head)
+	while (head)
 	{
 		trav = head->next;
 		free(head);
