493d5ce6ff7ead675efa40aaa15e62831e6fc24d
irafajs <irafajs@gmail.com>
2023-05-01 10:42:46 +0000

    MSLL07

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - write out the node data at passed node
+ * @head: passed arg as a pointer
+ * @index: passed int as a counter to the node index
+ *
+ * Return: NULL if index does not exist or data of node if index exists.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int kub;
+	listint_t *trav;
+
+	trav = head;
+	if (trav == NULL)
+	{
+		return (NULL);
+	}
+	while (trav != NULL)
+	{
+		if (kub == index)
+		{
+			return (trav);
+		}
+		kub++;
+		trav = trav->next;
+	}
+	return (NULL);
+}
