67b7dcb5eba39f496273de64a775e0a617670230
irafajs <irafajs@gmail.com>
2023-05-01 08:45:39 +0000

    MSLL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint_te - free the memory allocated by head
+ * free_listint2 - free the memory allocated by head
  * @head: passed arg as a pointer to a pinter
  *
  * Return: nothing
@@ -17,7 +17,7 @@ void free_listint2(listint_t **head)
 	while (*head != NULL)
 	{
 		trav = *head;
-		*head =trav->next;
+		*head = trav->next;
 		free(trav);
 	}
 	*head = NULL;
