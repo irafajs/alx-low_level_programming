9bcf487d9adbcf2856ad725d19d9b62e496172ea
irafajs <irafajs@gmail.com>
2023-05-14 10:59:48 +0000

    FIO02

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - append to the file the content
+ * @filename: passed arg as a pointer to the filepath
+ * @text_content: passed arg as a pointer to a text to append
+ *
+ * Return: 1 when successfull and -1 when error is returned
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fao, faw;
+	size_t c_count = 0;
+
+	fao = open(filename, O_WRONLY | O_APPEND);
+	if (fao == 0)
+	{
+		return (-1);
+	}
+	while (text_content[c_count] != '\0')
+	{
+	/*	faw = write(fao, text_content, c_count);*/
+		c_count++;
+	}
+	faw = write(fao, text_content, c_count);
+	if (faw == 0)
+	{
+		return (1);
+	}
+	else
+		return (-1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
 #define B_SIZE 1024
 
