564fa65d93792e33af17d63181bf9015015198d3
irafajs <irafajs@gmail.com>
2023-05-27 09:47:04 +0000

    FNL07

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,10 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		n = n * -1;
-
+		n = (n % 10) * -1;
 	}
 	_putchar ('0' + n % 10);
-	return (n % 10);
+	/*n = (n % 10) * -1;*/
+	/*printf("%d\n", n);*/
+	return (n);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <unistd.h>
+#include <limits.h>
 
 int _putchar(char c);
 void print_alphabet(void);
