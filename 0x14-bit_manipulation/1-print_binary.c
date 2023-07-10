3f9eb79b818b5f936621c9d3abcfdb1466bff6f1
irafajs <irafajs@gmail.com>
2023-07-10 19:52:50 +0000

    decimal to binary conversation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_binary - write out the converted desimanl to binary
+ * @n: passed argument representing passed decimal
+ *
+ * Retur: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	int size;
+	size_t op_res;
+	int  finess;
+	size_t one = 1;
+
+	if (n == 0)
+	{
+		_putchar ('0');
+		return;
+	}
+	size = sizeof(unsigned long int) * 8;
+	op_res = one << (size - 1);
+	finess = 1;
+	while (op_res != 0)
+	{
+		if ((n & op_res) != 0 || !finess)
+		{
+			printf("%d", (n & op_res) != 0);
+			finess = 0;
+		}
+		op_res >>= 1;
+	}
+}
