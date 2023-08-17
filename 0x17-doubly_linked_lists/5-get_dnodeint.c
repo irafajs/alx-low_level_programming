cdcb9d9c155bc6e4b39367eb960c158fd6da11a6
irafajs <irafajs@gmail.com>
2023-08-17 18:54:01 +0000

    DOUBLY LL T05_00

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,6 +13,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int count = 0;
 
 	temp = head;
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	while (head != NULL)
 	{
 		if (count == index)
