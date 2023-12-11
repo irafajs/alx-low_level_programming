b00a92a5db69b6d25955daa07300ba37390c7dfb
irafajs <irafajs@gmail.com>
2023-12-11 20:38:28 +0200

    SEARCH LINEAR T00

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function to search using linear search
+ * @array: pointer to the list of array
+ * @size: represent the length of the array
+ * @value: represent the value to serach for
+ *
+ * Return: value if success -1 at failure
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t ctr;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (ctr = 0; ctr < size; ctr++)
+	{
+		if (array[ctr] == value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", ctr, array[ctr]);
+			return (value);
+		}
+		if (array[ctr] != value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", ctr, array[ctr]);
+		}
+	}
+	return (-1);
+}
